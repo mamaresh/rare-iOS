757484f760ae4c6fd5578d394dfebac9a0c481b9
MANJUNATH AMARESH <Manu@manjunaths-macbook-pro.local>
2016-01-23 22:28:32 -0600

    Initial Commit

---
+++ rare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rare
+//
+//  Created by MANJUNATH AMARESH on 1/23/16.
+//  Copyright © 2016 MANJUNATH AMARESH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rare
+//
+//  Created by MANJUNATH AMARESH on 1/23/16.
+//  Copyright © 2016 MANJUNATH AMARESH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
